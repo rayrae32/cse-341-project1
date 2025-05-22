3c1bb94d13bdabb630f8e3f62108250fb805ab28
Rachel Ukpabi <ukpabirachel456@gmail.com>
2025-05-22 07:58:45 -0700

    kkk

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,5 @@
 const router = require('express').Router();
 
-router.use('/', require('./swagger'));
-
-router.get('/', (req, res) => {
-    //#swagger.tags=['Hello World]
-    res.send('Welcome to the API documentation!');
-});
-
-router.use('/users', require('./users'));
+router.get('/', (req, res) => {res.send('Hello World');});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,25 +1,8 @@
 const express = require('express');
-
-const mongodb = require('./data/database');
-const bodyParser = require('body-parser');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    next();
-});
 app.use('/', require('./routes'));
 
-mongodb.initDB((err) => {
-    if (err) {
-        console.log(err);
-    }
-    else {
-        app.listen(port, () => {console.log(`Database is listening and node Running on port ${port}`)});
-    }
-});
+app.listen(port, () => {console.log(`Running on port ${port}`)});
